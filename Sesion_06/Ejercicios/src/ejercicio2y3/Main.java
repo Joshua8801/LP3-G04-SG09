5e2da102f16abb38e64bd0873ff1de16869e6c4e
Joshua8801 <juan.hincho@estudiante.ucsm.edu.pe>
2025-10-06 06:05:21 -0500

    Ejercicio

---
+++ Sesion_06/Ejercicios/src/ejercicio2y3/Main.java
@@ -0,0 +1,10 @@
+package ejercicio2y3;
+
+public class Main {
+    public static void main(String[] args) {
+        InventarioModel modelo = new InventarioModel();
+        InventarioView vista = new InventarioView();
+        InventarioController controlador = new InventarioController(modelo, vista);
+        controlador.iniciar();
+    }
+}
